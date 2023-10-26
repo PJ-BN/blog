81347cda2a7ba3a23b9461b3001316221e74de06
PJ-BN <63279581+PJ-BN@users.noreply.github.com>
2023-10-26 19:55:33 +0545

    some small changes

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.starting_page, name = "starting-page"),
     path('post', views.post, name = "post"),
+    path('add', views.add, name = "add"),
+    path('adddata', views.adddata, name = "adddata"),
+    
     path('post/<slug:slug>', views.post_slug, name = "post-slug")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404 , redirect
+from .models import *
 
 
 def get_date(post):
@@ -20,3 +20,46 @@ def post_slug(request, slug):
     identify_post = get_object_or_404(Post, slug = slug)
     return render(request, "blog/post-detail.html", {"post": identify_post})
 
+def add(request):
+    
+    
+    return render(request, "blog/add.html")
+
+def adddata(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        firstname = request.POST.get("firstname")
+        lastname = request.POST.get("lastname")
+        email = request.POST.get("email")
+        tag = request.POST.get("tags")        
+        summary = request.POST.get("summary")        
+        content = request.POST.get("content")
+        author = Author.objects.values_list("email_address")
+        author = [i[0] for i in author]
+        if email not in author:
+            a = Author()
+            a.first_name = firstname
+            a.last_name = lastname
+            a.email_address = email
+            a.save()
+            
+        Tags = Tag.objects.values_list("caption")
+        Tags = [i[0] for i in Tags]
+        print(Tags)
+        if tag not in Tags:
+            t = Tag()
+            t.caption = tag
+            t.save()
+        p = Post()
+        p.title = title
+        p.excerpt = summary
+        p.content = content
+        # p.tags.add(tag, tag)
+        name = firstname + " " + lastname
+        print(name)
+        # p.author.add(name)
+        # p.save()
+        
+        
+        
+        return redirect( "add")
